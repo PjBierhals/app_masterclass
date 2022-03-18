3b842918de280df40c57d30ac271d8d7858ed38f
PjBierhals <pjbierhals@gmail.com>
2022-03-17 22:05:07 -0300

    icones

--- lib/pages/about/about_page.dart
+++ lib/pages/about/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:app_masterclass/components/app_bar.dart';
+
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatefulWidget {
@@ -16,6 +17,80 @@ class _AboutPageState extends State<AboutPage> {
         context: context,
         title: 'Sobre o dev',
         leading: Image.asset('assets/images/logo.png'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            color: const Color(0xFF172026),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://avatars.githubusercontent.com/u/68884037?v=4'),
+                    foregroundColor: Colors.transparent,
+                  ),
+                  const Text('Nome'),
+                  const Text('Descrição'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/images/awesome-facebook-f.png'),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Image.asset('assets/images/logo.png'),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Image.asset('assets/images/logo.png'),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Image.asset('assets/images/logo.png'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Text('Tecnologias Favoritas'),
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (BuildContext context, int index) {
+                  //var indexAux = index;
+                  return SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        color: const Color(0xFF172026),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset('assets/images/logo.png'),
+                              const Text("flutter"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          const Text('Habilidades e Competências'),
+        ],
       ),
     );
   }
